7e974e6acffd861ba7697730e79ae00be0e9d4b0
anttor <anttor062@gmail.com>
2019-09-22 13:27:52 -0600

    change funtions into class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello From React!</h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello From React!</h1>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    const name = "Asif Khan";
+    const showName = true;
+    const loading = false;
+
+    return (
+      <div className="App">
+        {!loading ? <h1>Hello {showName && name}</h1> : <h2>Loading...</h2>}
+      </div>
+    );
+  }
 }
 
 export default App;
