0b5e29985423725fb3e6a62f0e225a3821274816
anttor <anttor062@gmail.com>
2019-09-22 16:14:10 -0600

    Refactor navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
+import NavBar from "./components/layout/Navigation";
 import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello From React!</h1>
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
+class App extends Component {
   render() {
-    const name = "Asif Khan";
-    const showName = true;
-    const loading = false;
-
     return (
       <div className="App">
-        {!loading ? <h1>Hello {showName && name}</h1> : <h2>Loading...</h2>}
+        <NavBar title="Github Finder" icon="fab fa-github" />
       </div>
     );
   }
