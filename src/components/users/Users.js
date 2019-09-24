0cd725d7c9d0c4883fe1216d9d0cc34c4834aca6
asifdev <anttor062@gmail.com>
2019-09-24 13:55:53 -0600

    Update Users.js

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
 
-class Users extends Component {
-  //   state = {
-  //     users: [
-  //       {
-  //         id: "1614",
-  //         login: "brad",
-  //         avatar_url: "https://avatars0.githubusercontent.com/u/1614?v=4",
-  //         html_url: "https://github.com/brad"
-  //       },
-  //       {
-  //         id: "1616",
-  //         login: "brad",
-  //         avatar_url: "https://avatars2.githubusercontent.com/u/5550850?v=4",
-  //         html_url: "https://github.com/brad"
-  //       },
-  //       {
-  //         id: "1615",
-  //         login: "brad",
-  //         avatar_url: "https://avatars1.githubusercontent.com/u/2621?v=4",
-  //         html_url: "https://github.com/brad"
-  //       }
-  //     ]
-  //   };
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user}></UserItem>
         ))}
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: "grid",
