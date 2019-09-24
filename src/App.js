5e4b19dd01cad425a460a18a743c2a31fe43dc7d
asifdev <anttor062@gmail.com>
2019-09-24 14:21:03 -0600

    Add github authorization

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
     this.setState({
       loading: true
     });
-    const res = await axios.get("https://api.github.com/users");
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_FINDER_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_FINDER_CLIENT_secret}`
+    );
 
     this.setState({
       users: res.data,
