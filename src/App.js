7102dab83b70b216ee4301279bbea696e2198dc7
asifdev <anttor062@gmail.com>
2019-09-24 12:10:33 -0600

    Initiate axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import NavBar from "./components/layout/Navigation";
 import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  async componentDidMount() {
+    const res = await axios.get("https://api.github.com/users");
+    console.log(res.data);
+  }
+
   render() {
     return (
       <div className="App">
